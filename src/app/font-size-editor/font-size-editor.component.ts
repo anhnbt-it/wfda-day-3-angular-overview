4856414a7da4115c5a92c3e06745211aeaad516d
anhnbt-it <anhnbt.it@gmail.com>
2021-01-20 08:58:16 +0700

    first commit

---
+++ src/app/font-size-editor/font-size-editor.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-font-size-editor',
+  templateUrl: './font-size-editor.component.html',
+  styleUrls: ['./font-size-editor.component.css']
+})
+export class FontSizeEditorComponent implements OnInit {
+
+  fontSize = 14;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  onChange(event): void {
+    console.log(event.target.value);
+    this.fontSize = event.target.value;
+  }
+
+}
